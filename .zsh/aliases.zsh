103c365da9da53107c859d5f10cb51501f22730c
Henning Leth <leth.henning@gmail.com>
2023-01-22 13:13:30 +0100

    Add check for OS in aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,10 +1,16 @@
-if [ -x /usr/bin/dircolors  ]; then
-	test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
-	alias ls='ls --color=auto'
-	alias la='ls -lAh --color=auto'
-	alias ll='ls -alF --color=auto'
-
-	alias grep='grep --color=auto'
-	alias fgrep='fgrep --color=auto'
-	alias egrep='egrep --color=auto'
+if [[ "$OSTYPE" == "linux-gnu"* ]] then
+	if [ -x /usr/bin/dircolors  ]; then
+		test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+		alias ls='ls --color=auto'
+		alias la='ls -lAh --color=auto'
+		alias ll='ls -alF --color=auto'
+	
+		alias grep='grep --color=auto'
+		alias fgrep='fgrep --color=auto'
+		alias egrep='egrep --color=auto'
+	fi
+elif [[ "$OSTYPE" == "darwin"* ]] then
+	alias ls='ls -G'
+	alias la='ls -lAh -G'
+	alias ll='ls -alF -G'
 fi
