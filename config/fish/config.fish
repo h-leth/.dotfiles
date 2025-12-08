cc96564751d5d874ffd2d41f4db970f22eeff27e
Henning K. Leth <leth.henning@proton.me>
2025-12-08 19:41:19 +0100

    add bat as manpager

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,6 +4,7 @@ end
 
 set fish_greeting
 set TERM "xterm-256color"
+export MANPAGER="sh -c 'awk '\''{ gsub(/\x1B\[[0-9;]*m/, \"\", \$0); gsub(/.\x08/, \"\", \$0); print }'\'' | batcat -p -lman'"
 
 theme_gruvbox dark medium
 
